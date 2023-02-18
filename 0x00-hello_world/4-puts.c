c100eeb0e4c6f39218ccf7eee12ba69e067adb20
ayomide sufian <ayomidesufian@gmail.com>
2023-02-17 21:40:27 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - writing my first program
+ * main - writing my first program on c
  *
  * Return:0
 */
-int main < void >
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-		return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
