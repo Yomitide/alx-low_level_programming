2d4ab9ac60595763e62ef540a58ab7b29f59ddce
ayomide sufian <ayomidesufian@gmail.com>
2023-02-19 01:58:32 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,11 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 10; n++)
+	for (n = 0; n <= 9; n++)
 	{
-	putchar((n % 10) + '0');
-	if (n != 9)
-		continue;
-		putchar(',');
+		putchar(n);
+		if (n == 9)
+			putchar(',');
 		putchar(' ');
 	}
 	putchar('\n');
