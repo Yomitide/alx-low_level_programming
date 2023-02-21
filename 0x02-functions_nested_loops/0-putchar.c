b34f9757274e58a0abd37bb60caa87558b2706f6
ayomide sufian <ayomidesufian@gmail.com>
2023-02-21 10:16:46 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int _putchar('_');
+	int _putchar('p');
+	int _putchar('u');
+	int _putchar('t');
+	int _putchar('c');
+	int _putchar('h');
+	int _putchar('a');
+	int _putchar('r');
+	int _putchar('\n');
+	return (0);
+}
