bc0f9c381a6a93e65fac4e992b4648ba167897b0
ayomide sufian <ayomidesufian@gmail.com>
2023-02-21 17:22:43 -0800

    3-islowest.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print lowercase letter 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+
+	int i
+		i = 10;
+	while
+	(i <= 9);
+	{
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	_putchar(ch);
+	}
+	_putchar('\n');
+	i++;
+	}
+}
+
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase
+ * @c: the character to be checked
+ * Return: 1 for lowercase characte or 0 for anything  else
+ */
+int _islower(int c)
+{
+	if (c >= 92 && c<= 122)
+	{
+	return(1);
+	}
+	return(0);
+}
