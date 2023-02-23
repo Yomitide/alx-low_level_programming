168c170b9655245fc216186de1dc88d41e18baa0
ayomide sufian <ayomidesufian@gmail.com>
2023-02-23 00:47:05 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - print lowercase letter 10 times
- *
+ * Return: 0
  */
 void print_alphabet_x10(void)
 {
 	char ch;
 
-	int i
-		i = 10;
+	int i;
+		i = 0;
 	while
-	(i <= 9);
+		(i <= 9);
 	{
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
