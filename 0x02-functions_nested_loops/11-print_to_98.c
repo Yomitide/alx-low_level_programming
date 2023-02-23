babf90076c8cc6d09c0e59f9b3c59309602be50f
ayomide sufian <ayomidesufian@gmail.com>
2023-02-23 08:02:43 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (; n <=98; n++)
+		for (; n <= 98; n++)
 		{
 			printf("%d", n);
 			if (n != 98)
@@ -23,7 +23,7 @@ void print_to_98(int n)
 		for (; n >= 98; n--)
 		{
 			printf("%d", n);
-			if (n !=98)
+			if (n != 98)
 			{
 				printf(", ");
 			}
