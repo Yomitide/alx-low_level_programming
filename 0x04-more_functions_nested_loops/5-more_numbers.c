873b13009c4299bf8643ae3454e9587567b2660f
ayomide sufian <ayomidesufian@gmail.com>
2023-02-24 05:59:54 -0800

    main.h

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * more_number - print 0 to 14 x10 times
+ *
+ * Return: 0
+ */
+ void more_numbers(void)
+{
+	int x, y;
+	for (x = 0; x <= 14; x++)
+	{
+	for (y = 0; y <= 14; y++)
+	{
+	if (y > 9	
