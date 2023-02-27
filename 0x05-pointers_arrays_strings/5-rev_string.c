10b2a0a3496761a816f8552fa6824a304d451821
ayomide sufian <ayomidesufian@gmail.com>
2023-02-27 07:45:57 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - print a string in reverse
+ * @s: character in reverse
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+
+	index = 0;
+	char tmp;
+
+	while (s(index))
+		len++;
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		s(index) = s(len - index - 1);
+		s(len - index - 1) = tmp;
+	}
+}
