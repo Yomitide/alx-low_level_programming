1761e220e51c1f25b381191c40c4648675075c0a
ayomide sufian <ayomidesufian@gmail.com>
2023-03-02 15:33:16 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,8 +17,8 @@ char *_strcat(char *dest, char *src)
 	}
 	j = 0;
 	while (src[j] != '\0')
-	(dest[i] = src[j]);
 	{
+	(dest[i] = src[j]);
 		i++;
 		j++;
 	}
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * *cap_string - capitalise all words of a string
+ * Return: a pointer to the string
+ */
+char *cap_string(char *)
+{
+
