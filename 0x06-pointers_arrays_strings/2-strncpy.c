9943e0c84e117a9156830375a7374ade9fad945b
ayomide sufian <ayomidesufian@gmail.com>
2023-03-02 02:48:16 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,15 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (i < n && src[i] != '\0')
 	{
-	if (s1[i] != s2[i])
-		{
-	return (s1[i] = s2[i]);
-			}
+	dest[i] = src[i];
+	i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
 			i++;
-			return (0);
-			}
-			}
+	}
+			return (dest);
+}
