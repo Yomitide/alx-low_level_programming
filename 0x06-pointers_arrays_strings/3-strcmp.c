b6fa0f19a6f69f9d8374cb9a11809f1829b6fbd9
ayomide sufian <ayomidesufian@gmail.com>
2023-03-02 16:06:26 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,13 @@ int _strcmp(char *s1, char *s2)
 {
 	int c = 0;
 
-	while (s1[c] == s2[c])
+	while (s1[c] != '\0' && s2[c] != '\0')
 	{
-	if (s1[c] == '\0' || s2[c] == '\0')
-		break;
+	if (s1[c] != s2[c])
+	{
+		return (s1[c] - s2[c]);
+	}
 	c++;
 	}
-	if (s1[c] == '\0' && s2[c] == '\0')
 		return (0);
-	else
-		return (-1);
 }
