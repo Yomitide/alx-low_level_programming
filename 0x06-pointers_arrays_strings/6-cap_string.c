2ec9051162aa06cc18238699c87060ba6a491671
ayomide sufian <ayomidesufian@gmail.com>
2023-03-02 17:34:40 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,8 +1,33 @@
 #include "main.h"
 /**
  * *cap_string - capitalise all words of a string
+ * @str: the useless string
  * Return: a pointer to the string
  */
 char *cap_string(char *)
 {
+	int i = 0;
 
+	while (str[i])
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+		if (str[i - 1] == ' ' 
+		str[i - 1] == '\t'
+		str[i - 1] == ','
+		str[i - 1] == ';'
+		str[i - 1] == '\n'
+		str[i - 1] == '.'
+		str[i - 1] == '!'
+		str[i - 1] == '?'
+		str[i - 1] == '"'
+		str[i - 1] == '('
+		str[i - l] == '}'
+		str[i - 1] == ')'
+		str[i - 1] == '{'
+		i == 0)
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
