09727aeb70cd18b1d28c7988f98235f39d2542d8
ayomide sufian <ayomidesufian@gmail.com>
2023-03-06 08:48:54 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -10,15 +10,14 @@
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-	unsigned int i;
+        unsigned int i;
 
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-  
-		{
-		printf(" ");
+        i = 0;
+        while (i < size)
+        {
+                if (i % 10)
+                {
+                        printf(" ");
                 }
                 if (!(i % 10) && i)
                 {
@@ -37,3 +36,11 @@ void simple_print_buffer(char *buffer, unsigned int size)
  */
 int main(void)
 {
+    char buffer[98] = {0x00};
+
+    simple_print_buffer(buffer, 98);
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);    
+    return (0);
+}
