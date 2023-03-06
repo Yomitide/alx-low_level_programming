b2bb29018c7e82fafb6d7d376ae5ff9aeae167df
ayomide sufian <ayomidesufian@gmail.com>
2023-03-06 15:00:12 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,21 +8,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n, value, check;
+	unsigned int n = 0;
+	int i;
 
-	value = 0;
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		check = 0;
-	for (n = 0; accept[n] != '\0'; n++)
-	{
-	if (accept[n] == s[i])
-		}
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
 			{
-			value++;
-			check = 1;
+				n++;
+				break;
 			}
-			}
-	if (check == 0)
-		return (0);
+			else if (accept[i + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
 }
