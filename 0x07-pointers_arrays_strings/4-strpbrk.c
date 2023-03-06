6784436c0ef2b0161e0134584e3113f1f3a9e656
ayomide sufian <ayomidesufian@gmail.com>
2023-03-06 09:37:05 -0800

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strpbrk - search a string for any st of byte
+ * @s: pointer parameter
+ * @accept: pointer parameter
+ * Return: 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	for (n = 0; accept[n] != '\0'; n++)
+		{
+	if (s[i] == accept[n])
+		return (s + i);
+		}
+	}
+	return (0);
+}
