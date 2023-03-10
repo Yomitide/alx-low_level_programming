6e9741ff49586bb30d9d664845b4a9ca3d311dd4
ayomide sufian <ayomidesufian@gmail.com>
2023-03-10 15:08:28 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - the main program
+ * @argc: the arguement
+ * @argv: the pointer to the array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+		return (0);
+}
