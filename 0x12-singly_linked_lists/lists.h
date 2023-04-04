a9f06809943d00b5910ebbf3f5786977b61786fd
ayomide sufian <ayomidesufian@gmail.com>
2023-04-04 08:01:14 -0700

    My readme file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,10 +4,9 @@
 #include <stdio.h>
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
+ * @str: string - malloc'ed string
  * @len: length of the string
  * @next: points to the next node
- *
  * Description: singly linked list node structure
  * for Holberton project
  */
