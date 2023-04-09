22ed8203bcd2b8e3ebe98112b3be55d057de2f8f
ayomide sufian <ayomidesufian@gmail.com>
2023-04-09 06:24:20 -0700

    check for endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_endianness - check for endiannes
+ * Return: 0 if big, 1 if little
+ */
+int get_endianness(void)
+{
+	unsigned int x;
+		char *c =(char *) &x;
+
+	if (*c)
+	{
+	return (1);
+	}
+	return (0);
+}
