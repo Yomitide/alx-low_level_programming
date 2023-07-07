0a8d2ac9ec7ca3bd4fb48668f8877279f9100439
Ayomide <ayomidesufian@gmail.com>
2023-07-07 14:18:55 -0700

    first task on bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,17 +7,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int x = 0;
-	unsigned int y = 0;
+	unsigned int n = 0, p = 1;
+	int len;
 
-	if (b[x] == 0)
+	if (b == NULL)
 		return (0);
-	while ((b[x] == '0') || (b[x] == '1'))
-	{
-		y <<= 1;
-		y += b[x] - '0';
-		x++;
-	}
+	for (len = 0; b[len];)
+		len++;
 
-	return (y);
+	for (len -= 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		n += (b[len] - '0') * p;
+		p *= 2;
+	}
+	return (n);
 }
