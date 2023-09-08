ccf4a50ec88f7b0b3cf0f6bc9967a0748a16d679
yomitide <ayomidesufian@gmail.com>
2023-09-08 09:45:56 -0700

    readme and header file

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,68 @@
+/**
+ * add - to (+) two numbers.
+ *
+ * @a:first number.
+ * @b:second number.
+ *
+ * Return: a+b;
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * sub - to (-) two numbers.
+ *
+ * @a:first number.
+ * @b:second number.
+ *
+ * Return: a-b;
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * mul - to (*) two numbers.
+ *
+ * @a:first number.
+ * @b:second number.
+ *
+ * Return: a*b;
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * div - to (/) two numbers.
+ *
+ * @a:first number.
+ * @b:second number.
+ *
+ * Return: a/b;
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * mod - to (%) two numbers.
+ *
+ * @a:first number.
+ * @b:second number.
+ *
+ * Return: a%b;
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
